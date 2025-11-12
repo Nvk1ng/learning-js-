68fbcd6ce3644999d73138ef63e6645de7e13b91
matheus sanderhus <sanderhus15@outlook.com>
2025-11-12 05:05:22 -0300

    special number and strings

---
+++ 4_strings/scripts.js
@@ -0,0 +1,7 @@
+console.log("isso é uma string");
+console.log("Isso é uma string");
+console.log(`Isso é uma string`);
+
+console.log(typeof "isso é uma string");
+console.log(typeof "Isso é uma string");
+console.log(typeof `Isso é uma string`);
