9ad44da84c2f25ca10c9fbdb1d5c60bf9100d249
matheus sanderhus <sanderhus15@outlook.com>
2025-11-12 14:09:18 -0300

    variaveis em js

---
+++ 1_variaveis/scripts.js
@@ -0,0 +1,21 @@
+let nome = "matheus";
+console.log(nome);
+console.log(`o meu nome é ${nome}`);
+
+let laranjas = 5;
+console.log(laranjas * laranjas);
+
+nome = "joao";
+console.log(nome);
+
+laranjas = 313123;
+console.log(laranjas);
+
+laranjas = "laranjas";
+console.log(laranjas);
+
+let um = 1,
+  dois = 2,
+  tres = 3;
+
+console.log(um + dois + tres);
