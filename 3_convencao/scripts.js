76ae21eeba1eb3f94d060ef649649f07d628953b
matheus sanderhus <sanderhus15@outlook.com>
2025-11-12 14:09:49 -0300

    convensao de variaveis

---
+++ 3_convencao/scripts.js
@@ -0,0 +1,10 @@
+let nome3 = "teste";
+let $nome = "teste2";
+let _nome = "teste3";
+let Nome = "teste4";
+let seuNome = "teste5";
+
+//Nao funciona
+// let 3nome = "nome";
+// let @nome = "nome";
+// let !nome = "nome";
