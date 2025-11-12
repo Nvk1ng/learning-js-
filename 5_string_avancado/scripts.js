9d6188cd71146035c39742e8c9d96cd077f95809
matheus sanderhus <sanderhus15@outlook.com>
2025-11-12 05:10:33 -0300

    strings avancados e comparacoes

---
+++ 5_string_avancado/scripts.js
@@ -0,0 +1,5 @@
+console.log("Primeira linha \nsegunda linha");
+console.log("O meu nome é 'Matheus'");
+console.log('O meu nome é "Matheus"');
+console.log(`A multiplcaçao de 5 por 3 é ${5 * 3}`);
+console.log("O " + "meu " + "nome " + "é " + "Matheus");
