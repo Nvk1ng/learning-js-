f3cd42a010327ad7797424de4bc12dcd47d68e1e
matheus sanderhus <sanderhus15@outlook.com>
2025-11-12 05:06:53 -0300

    operadores logicos and conversao automatica

---
+++ 8_operadores_logicos/scripts.js
@@ -0,0 +1,12 @@
+//operador logico and (&&) -> so pode ser true se ambos os operandos forem true
+console.log(true && true);
+console.log(false && true);
+
+//operador logico or (||) -> pode ser true se ao menos um dos operandos for true
+console.log(false || true);
+console.log(false || false);
+console.log(true || true);
+
+//operador logico not (!) -> inverte o valor logico do operando
+console.log(!true);
+console.log(!false);
