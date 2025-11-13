48d5f21435d24993a9b10535f985421918e4fbbb
matheus sanderhus <sanderhus15@outlook.com>
2025-11-13 15:08:43 -0300

    funcao alert e math

---
+++ 7_math/scripts.js
@@ -0,0 +1,11 @@
+let maior = Math.max(5, 12, 28, 30, 2);
+console.log(maior);
+
+let menor = Math.min(5, 12, 28, 30, 2);
+console.log(menor);
+
+let arredondar = Math.round(1.291831);
+console.log(arredondar);
+
+let arredondarParaCima = Math.ceil(5.293123);
+console.log(arredondarParaCima);
