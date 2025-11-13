c8052e36585d47dec18fd22b74b30db03923934d
matheus sanderhus <sanderhus15@outlook.com>
2025-11-13 15:08:55 -0300

    if

---
+++ 8_if/scripts.js
@@ -0,0 +1,12 @@
+let idade = 19;
+
+if (idade >= 18) {
+  console.log("Pode entrar");
+}
+
+idade = 25;
+let nome = "Matheus";
+
+if (idade > 18 && nome == "matheus") {
+  console.log("pode entrar");
+}
