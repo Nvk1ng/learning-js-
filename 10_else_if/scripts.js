70d521a69cc0d0c9eb6d76cfc5b99e574b013470
matheus sanderhus <sanderhus15@outlook.com>
2025-11-17 10:49:55 -0300

    if, else, else if

---
+++ 10_else_if/scripts.js
@@ -0,0 +1,12 @@
+let a = 5;
+let b = 3;
+
+if (a + b == 3) {
+  console.log("o resultado é 3");
+} else if (a == 4) {
+  console.log("o resultado é 4");
+} else if (b == 3) {
+  console.log("o resultado é 3");
+} else {
+  console.log("nenhuma das condiçoes acimas");
+}
