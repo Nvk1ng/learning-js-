aa5bf3c8ea60f9cbf54f952cccaf86e88c4ee41b
matheus sanderhus <sanderhus15@outlook.com>
2025-11-18 15:33:46 -0300

    break

---
+++ 15_break/scripts.js
@@ -0,0 +1,11 @@
+let nome = "matheus";
+for (let i = 0; i < 10; i += 1) {
+  if (i == 3) {
+    nome = "jaoo";
+  }
+  if (i == 5 && nome == "jaoo") {
+    console.log("o nome é joao");
+    brak;
+  }
+  console.log(i);
+}
