41385e85200e798568626e5b05b462590bb8ee29
matheus sanderhus <sanderhus15@outlook.com>
2025-11-18 10:21:42 -0300

    while

---
+++ 11_while/scripts.js
@@ -0,0 +1,17 @@
+let x = 10;
+while (x > 0) {
+  console.log(x);
+  x -= 1;
+}
+
+let y = 0;
+while (y <= 10) {
+  console.log(y);
+  y += 1;
+}
+
+// Exemplo de loop infinito
+// let z = 10;
+// while (z > 0) {
+//   console.log(z);
+// }
