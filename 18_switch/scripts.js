0ca232b6506a5ac6f9dc6104f42ca823eea172cd
matheus sanderhus <sanderhus15@outlook.com>
2025-11-21 09:56:51 -0300

    switch

---
+++ 18_switch/scripts.js
@@ -0,0 +1,13 @@
+let nome = "matheus";
+
+switch (nome) {
+  case "joao":
+    console.log("Seu nome é joao");
+    break;
+  case "marcos":
+    console.log("seu nome é marcos");
+    break;
+  default:
+    console.log("seu nome nao é joao e nem marcos");
+    break;
+}
