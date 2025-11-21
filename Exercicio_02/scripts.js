e8e5960b22e96cc96966b22b14bc52bcb2fed1aa
matheus sanderhus <sanderhus15@outlook.com>
2025-11-21 14:21:05 -0300

    ex 02

---
+++ Exercicio_02/scripts.js
@@ -0,0 +1,13 @@
+/*Cria uma estrutura if que verifica a entrada na baalda, se tiver
+mais de 18 anos pode entrar;
+armazenar a idade em uma variavel com let;
+insira uma instrucao console.log("pode entrar"), caso a pessoa tenha mais que 18 anos
+*/
+
+let idade = 18;
+
+if (idade >= 18) {
+  console.log("Pode entrar");
+} else {
+  console.log("Nao pode entrar");
+}
