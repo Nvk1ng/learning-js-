29e83403cc1fca39c2c1789c132c5aa77b65d7b6
matheus sanderhus <sanderhus15@outlook.com>
2025-11-21 14:21:00 -0300

    ex 01

---
+++ Exercicio_01/scripts.js
@@ -0,0 +1,13 @@
+/*Armazene em valores em variaveis com cada um dos tipos de dados vistos
+String,  number e boolean 
+*/
+
+let numero = 1;
+let nome = "Matheus";
+let verdadeiro = true;
+let falso = false;
+
+console.log(typeof numero);
+console.log(typeof nome);
+console.log(typeof verdadeiro);
+console.log(typeof falso);
