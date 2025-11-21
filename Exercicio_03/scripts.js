633826a8e8afea670bb9376987b5aa8943593e40
matheus sanderhus <sanderhus15@outlook.com>
2025-11-21 14:21:11 -0300

    ex 03

---
+++ Exercicio_03/scripts.js
@@ -0,0 +1,9 @@
+/* cria uma constante com o seu nome como valor;
+depois uma estrutura if que verifica se o seu nome é o que esta na constante;
+se estiver, emita uma mensagem de saudaçao com console.log()
+ */
+
+const nome = "Matheus";
+if (nome == "Matheus") {
+  console.log("Ola Matheus");
+}
