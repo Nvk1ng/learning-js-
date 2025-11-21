ebaf871f996b47e6d2593160b4d9c24a6b5e708b
matheus sanderhus <sanderhus15@outlook.com>
2025-11-21 10:11:23 -0300

    exercicio 05

--- Tipos_de_dados/Exercicios_01/scripts.js
+++ Tipos_de_dados/Exercicios_01/scripts.js
@@ -3,25 +3,3 @@
 console.log("Hello World!");
 console.log("Hello World!");
 console.log(`Hello World!`);
-
-//Escreva tres valores em number em um arquivo e exiba no navegador com console.log()
-//Com numeros inteiros, numeros com casa decimal e por aritmética
-console.log(4);
-console.log(4.88);
-console.log(2 * 4);
-
-//Escreva tres operacoes com boolean
-//Uma com maior, menor ou igual e diferente
-console.log(3 > 2);
-console.log(2 <= 2);
-console.log(5 != 6);
-
-//Escreva tres operacoes com operadores logicos
-// com and, or e not
-console.log(true && true);
-console.log(true || false);
-console.log(!true);
-
-//Faça uma operaçao que emita NaN no console do navegador
-console.log("dois" * "tres");
-console.log("dois" * 3);
