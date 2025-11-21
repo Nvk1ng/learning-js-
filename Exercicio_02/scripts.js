ba50ebd6499ef7ac17b46eb50b59caeaf2e137a9
matheus sanderhus <sanderhus15@outlook.com>
2025-11-21 10:10:45 -0300

    exercicio 02

---
+++ Exercicio_02/scripts.js
@@ -0,0 +1,7 @@
+/*Escreva tres valores em number em um arquivo e exiba no navegador com console.log()
+Com numeros inteiros, numeros com casa decimal e por aritmética
+*/
+
+console.log(4);
+console.log(4.88);
+console.log(2 * 4);
