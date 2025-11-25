e83fc2a28d57c046c6b2066e40cc92170eb6ac8f
matheus sanderhus <sanderhus15@outlook.com>
2025-11-25 14:49:02 -0300

    funcoes

---
+++ 01_definir_funcoes/scripts.js
@@ -0,0 +1,21 @@
+function imprimirNoConsole() {
+  console.log("Ola mundo!");
+}
+
+imprimirNoConsole();
+
+function imprimirUmNumero(num) {
+  console.log("O numero é " + num);
+}
+
+imprimirUmNumero(2);
+imprimirUmNumero(5);
+imprimirUmNumero(10);
+imprimirUmNumero(200);
+imprimirUmNumero(3);
+
+const numeroAleatorio = function () {
+  console.log(Math.random());
+};
+
+numeroAleatorio();
