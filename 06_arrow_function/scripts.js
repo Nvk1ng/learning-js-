2f056d68e0aa3fb802de84ba11c0d4f9a5dcf8b3
matheus sanderhus <sanderhus15@outlook.com>
2025-12-08 17:37:22 -0300

    arrow function

---
+++ 06_arrow_function/scripts.js
@@ -0,0 +1,17 @@
+const parOuImpar = (n) => {
+  return n % 2;
+};
+
+console.log(parOuImpar(3));
+
+let consoleTeste = () => {
+  console.log("ola");
+};
+
+console.log(consoleTeste());
+
+let soma = (a, b) => {
+  return a + b;
+};
+
+console.log(soma(2, 2));
