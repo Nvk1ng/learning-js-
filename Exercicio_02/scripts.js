3bbee7eefe529bcbd8bb22723fe706d6f74756c2
matheus sanderhus <sanderhus15@outlook.com>
2025-12-10 11:06:56 -0300

    exercicio 02

---
+++ Exercicio_02/scripts.js
@@ -0,0 +1,10 @@
+/*Escreva uma funçao que recebe um parametro de idade
+e imprima esta mensagem no console com template literals dizendo
+voce tem x anos
+*/
+
+function idade(n) {
+  console.log(`voce tem ${n} anos`);
+}
+
+idade(32);
